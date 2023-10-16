123eef40c7628fae122f0827d9f3740cddb14568
Gleb Kaplunskiy <gphenomen@gmail.com>
2023-10-16 11:40:38 +0300

    rewrite tests, trying Jest

--- tests/capitalize.test.js
+++
@@ -1,8 +0,0 @@
-import capitalize from '../src/capitalize.js';
-import { strict as assert } from 'node:assert';
-
-assert(capitalize('hello') === 'Hello');
-
-assert(capitalize('') === '');
-
-console.log('Все тесты пройдены!');
